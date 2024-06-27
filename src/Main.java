f9cd498a7d0bf98c8fc949add4b8b404a9b753dd
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 15:21:25 -0400

    Completed Find the Length of a String

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+        int textLength = text.length();
+        System.out.println("Text Length is " + textLength);
+    }
+}
